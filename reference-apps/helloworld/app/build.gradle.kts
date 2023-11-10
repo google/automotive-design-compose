efc4c706498e0492eb64cc83019588448d1c3b4d
Tim Froehlich <froeht@google.com>
2023-11-10 12:22:01 -0700

    Merge branch 'main' into feature/rustlayout

---
+++ reference-apps/helloworld/app/build.gradle.kts
@@ -0,0 +1,110 @@
+import org.apache.tools.ant.taskdefs.condition.Os
+
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+// Hacky: GH-502
+import com.android.designcompose.cargoplugin.CargoBuildType
+import com.android.designcompose.cargoplugin.getHostCargoOutputDir
+
+evaluationDependsOn(":designcompose")
+// End Hacky
+plugins {
+    kotlin("android")
+    id("com.android.application")
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.designcompose)
+}
+
+java { toolchain { languageVersion.set(JavaLanguageVersion.of(11)) } }
+
+var applicationID = "com.android.designcompose.testapp.helloworld"
+
+@Suppress("UnstableApiUsage")
+android {
+    namespace = applicationID
+    compileSdk = libs.versions.compileSdk.get().toInt()
+
+    defaultConfig {
+        applicationId = applicationID
+        minSdk = libs.versions.appMinSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        vectorDrawables.useSupportLibrary = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    buildFeatures { compose = true }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.androidx.compose.compiler.get()
+    }
+
+    // Deprecated in AGP 8+, replaced by `packaging`
+    @Suppress("DEPRECATION")
+    packagingOptions { resources { excludes.add("/META-INF/{AL2.0,LGPL2.1}") } }
+
+    testOptions.unitTests {
+        isIncludeAndroidResources = true // For Roborazzi
+    }
+}
+
+afterEvaluate {
+    if (Os.isFamily(Os.FAMILY_WINDOWS)) {
+        tasks.withType<Test>().configureEach {
+            logger.warn("Robolectic native tests not suppoted on Windows, disabling $name")
+            enabled = false
+        }
+    }
+}
+
+dependencies {
+    implementation(libs.designcompose)
+    ksp(libs.designcompose.codegen)
+
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.compose.material)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.material)
+
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    testImplementation(libs.robolectric)
+    testImplementation(libs.roborazzi)
+    testImplementation(libs.roborazzi.compose)
+    testImplementation(libs.roborazzi.junit)
+    testImplementation(libs.androidx.test.espresso.core)
+    testImplementation(libs.androidx.compose.ui.test.junit4)
+}

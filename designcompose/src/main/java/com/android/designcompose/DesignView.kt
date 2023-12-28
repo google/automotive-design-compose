63fea7f1c1066e509076f50a6cb11b48ffab8a21
Tim Froehlich <froeht@google.com>
2023-12-28 21:32:28 +0000

    Fix "Click interactions require two clicks #553" (#631)
    I'm not confident that this is the best fix, but it does seem to work.
    By making the coroutine start undispatched it runs immediately and
    handles the click properly again.
    
    I've added a unit test to exercise some of the interaction tests. More
    can be added later.

--- designcompose/src/main/java/com/android/designcompose/DesignView.kt
+++ designcompose/src/main/java/com/android/designcompose/DesignView.kt
@@ -86,6 +86,7 @@ import com.android.designcompose.serdegen.ViewData
 import com.android.designcompose.serdegen.ViewStyle
 import kotlin.math.min
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.CoroutineStart
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
@@ -502,7 +503,7 @@ internal fun DesignView(
                     // event so that the interaction state can revert the change).
                     //
                     // The interaction test document covers all of these cases.
-                    interactionScope.launch {
+                    interactionScope.launch(start = CoroutineStart.UNDISPATCHED) {
                         beginSection("DesignView InteractionScope")
                         detectTapGestures(
                             onPress = {

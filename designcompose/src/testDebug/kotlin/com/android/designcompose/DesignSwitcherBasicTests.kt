9fd75750f1e32d6018273e3ba98792d9d12dbd95
Tim Froehlich <froeht@google.com>
2024-02-09 12:43:50 -0700

    Add a basic DesignSwitcher Roborazzi test
    It will be used as a base to test basic testTags

---
+++ designcompose/src/testDebug/kotlin/com/android/designcompose/DesignSwitcherBasicTests.kt
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.designcompose
+
+import androidx.activity.ComponentActivity
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.android.designcompose.test.internal.defaultRoborazziRule
+import com.github.takahirom.roborazzi.RobolectricDeviceQualifiers
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
+import org.robolectric.annotation.GraphicsMode
+
+@Composable
+fun DesignSwitcherDeadbeef() {
+    DesignSwitcher(doc = null, currentDocId = "DEADBEEF", branchHash = null, setDocId = {})
+}
+
+@RunWith(AndroidJUnit4::class)
+@GraphicsMode(GraphicsMode.Mode.NATIVE)
+@Config(qualifiers = RobolectricDeviceQualifiers.MediumTablet, sdk = [34])
+class DesignSwitcherBasicTests {
+    @get:Rule val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    @get:Rule val roborazziRule = defaultRoborazziRule(composeTestRule)
+
+    @Test
+    fun displaysMiniVersion() {
+        with(composeTestRule) { setContent { DesignSwitcherDeadbeef() } }
+    }
+}

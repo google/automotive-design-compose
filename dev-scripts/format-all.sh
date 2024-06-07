34c56134956bfd78b842b2a5a4bce6555bc27769
Tim Froehlich <froeht@google.com>
2024-06-07 17:00:58 +0000

    Exclude editor configuration and Rust target directories from addlicense (#1207)
    These are built or generated files and don't need licenses.
    
    Also remove `.idea/misc.xml` from version control as it was being
    regenerated on project open.

--- dev-scripts/format-all.sh
+++ dev-scripts/format-all.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# Copyright 2023 Google LLC
+#
+# Copyright 2024 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,6 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
 set -e
 
@@ -27,8 +29,11 @@ if ! which "$GOPATH"/addlicense; then
 fi
 
 "$GOPATH"/addlicense \
+    -ignore "**/.idea/**" \
+    -ignore "**/.vscode/**" \
     -ignore "**/.gradle/**" \
     -ignore "**/build/**" \
+    -ignore "**/target/**" \
     -c "Google LLC" \
     -l apache \
     "$GIT_ROOT"

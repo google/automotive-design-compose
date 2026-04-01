01b7702da9501314d4a2e337f80d8afe38ccda34
Marcell Kovacs <marcellk@google.com>
2026-04-01 10:18:53 -0700

    Use the log crate to print error logs in crates/dc_figma_import/src/bin/fetch.rs

--- crates/dc_figma_import/src/bin/fetch.rs
+++ crates/dc_figma_import/src/bin/fetch.rs
@@ -21,7 +21,7 @@ fn main() {
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
     let args = Args::parse();
     if let Err(e) = fetch(args) {
-        eprintln!("Fetch failed: {:?}", e);
+        log::error!("Fetch failed: {:?}", e);
         std::process::exit(1);
     }
 }

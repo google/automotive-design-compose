2e5ea5aab5b6cea60b97b7e75b4bfa8f5c4946f0
Marcell Kovacs <marcellk@google.com>
2026-04-13 18:55:28 -0700

    Remove global eslint-disable in PlaybackController.ts as suggested by anshu10sep

--- support-figma/dc_squoosh_designer/src/timeline/PlaybackController.ts
+++ support-figma/dc_squoosh_designer/src/timeline/PlaybackController.ts
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import { AnimationData, Node, Timeline, Keyframe, SerializedNode, Variant, KeyframeTime, AnimatedNode, AnimationNode } from "./types";
 import { EventEmitter } from "./EventEmitter";
 import { InterpolationService } from "../utils/InterpolationService";

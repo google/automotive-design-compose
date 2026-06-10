162523e5b407a40be348938b39336aba2681a514
Gurupreet Singh <gurusingh@google.com>
2026-06-10 09:32:53 +0000

    Fix test regression by stripping semantics from early return modifier

--- designcompose/src/main/java/com/android/designcompose/squoosh/SquooshRoot.kt
+++ designcompose/src/main/java/com/android/designcompose/squoosh/SquooshRoot.kt
@@ -282,7 +282,10 @@ fun SquooshRoot(
 
     if (doc == null) {
         Log.d(TAG, "No doc! $docName / $incomingDocId")
-        Box(modifier) {
+        val cleanModifier = modifier.foldIn<Modifier>(Modifier) { acc, element ->
+            if (element is androidx.compose.ui.semantics.SemanticsModifier) acc else acc.then(element)
+        }
+        Box(cleanModifier) {
             designSwitcher()
         }
         return
